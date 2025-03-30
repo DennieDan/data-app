8860a817f8908ae6f15834f45772e15bb0d6f242
DennieDan <e1129903@u.nus.edu>
2025-03-30 16:00:07 +0800

    Add text input for nrows view

--- app.py
+++ app.py
@@ -22,8 +22,19 @@ if uploaded_file:
         sheet_name = st.selectbox("Select a sheet", xls.sheet_names)
         df = pd.read_excel(xls, sheet_name=sheet_name)
 
+    # Get basic data
+    df_nrows = len(df)
+
     # Display top N rows
-    n_rows = st.slider("Select number of rows to display", min_value=5, max_value=50, value=10)
+    def update_slider():
+        st.session_state.slider = st.session_state.numeric
+    def update_numin():
+        st.session_state.numeric = st.session_state.slider
+    
+    val = st.number_input('Input', value = 0, key = 'numeric', on_change=update_slider)
+    n_rows = st.slider("Select number of rows to display", value = val, key = 'slider', 
+                       min_value=0, max_value=df_nrows,
+                       on_change=update_numin)
     st.dataframe(df.head(n_rows))
 
     # **LIDA AI Analysis**
